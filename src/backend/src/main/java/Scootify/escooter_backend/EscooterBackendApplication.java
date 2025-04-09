f3efec7986803a22a51c74d41f14617276e3223a
David Feng <110443929+DavidFeng-8844@users.noreply.github.com>
2025-04-09 23:56:43 +0800

    update backend

---
+++ src/backend/src/main/java/Scootify/escooter_backend/EscooterBackendApplication.java
@@ -0,0 +1,13 @@
+package Scootify.escooter_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscooterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscooterBackendApplication.class, args);
+	}
+
+}
---
+++ src/backend/src/test/java/Scootify/escooter_backend/EscooterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Scootify.escooter_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscooterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
